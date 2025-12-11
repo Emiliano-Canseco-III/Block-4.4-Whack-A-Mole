45df83878a4188ad2cdc3aa89d572fb364b55992
Milo Canseco <canseco3rd@gmail.com>
2025-12-10 20:59:33 -0700

    Started creating html in the JS function 'app'.

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,4 +1,19 @@
-// import { useState } from "react";
-// import { holes } from "../data.js";
+import { useState } from "react";
+import { holes } from "../data.js";
 
-export default function App() {}
+export default function App() {
+  const [holeData, setHoleData] = useState(holes);
+
+  return (
+    <main>
+      <h1>Whack-A-Mole</h1>
+      <section className="game-board">
+        {holeData.map((hole) => (
+          <div key={hole.id} className="hole">
+            {hole.hasMole ? "🐹" : ""}
+          </div>
+        ))}
+      </section>
+    </main>
+  );
+}
