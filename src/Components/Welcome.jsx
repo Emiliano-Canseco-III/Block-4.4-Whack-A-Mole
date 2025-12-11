8d823b8b64b99feacea72c71762bdc2e8387b4b1
Milo Canseco <canseco3rd@gmail.com>
2025-12-10 21:29:55 -0700

    Created html to display a welcome message and button to click play.

---
+++ src/Components/Welcome.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useGame } from "GameContext.jsx";
+
+export default function Welcome() {
+  const { startGame } = useGame();
+
+  return (
+    <section className="Welcome">
+      <h2>Welcome to Whack-A-Mole!</h2>
+      <p>Click Play to start. Whack the mole to score points!</p>
+      <button onClick={startGame}>Play</button>
+    </section>
+  );
+}
