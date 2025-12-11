28c41d97aacf2bd22399ef31bb165afac27b83ca
Milo Canseco <canseco3rd@gmail.com>
2025-12-10 17:42:04 -0700

    Added 9 holes in the holes array.

--- src/data.js
+++ src/data.js
@@ -1,1 +1,11 @@
-export const holes = [];
+export const holes = [
+  { id: 1, hasMole: false },
+  { id: 2, hasMole: false },
+  { id: 3, hasMole: false },
+  { id: 4, hasMole: false },
+  { id: 5, hasMole: false },
+  { id: 6, hasMole: false },
+  { id: 7, hasMole: false },
+  { id: 8, hasMole: false },
+  { id: 9, hasMole: false },
+];
